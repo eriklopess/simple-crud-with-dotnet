46c1c278a3e3d3e11001c2047c139fda1fa0abd8
Erik Lopes <erik.lopesoliveira2019@gmail.com>
2022-08-08 04:16:55 -0300

    finalizando crud

---
+++ Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/{code}", ([FromRoute] string code) => ProductRepository.GetBy(code));
+app.MapPost("/", (Product product) => ProductRepository.Add(product));
+app.MapPut("/", (Product product) => ProductRepository.UpdateProduct(product));
+app.MapDelete("/{code}", ([FromRoute] string code) => {
+    Product product = ProductRepository.GetBy(code);
+    ProductRepository.RemoveProduct(product);
+});
+
+app.Run();
+
+public static class ProductRepository {
+    public static List<Product> Products { get; set; }
+
+    public static void Add(Product product) {
+        if(Products == null)
+            Products = new List<Product>();
+        
+        Products.Add(product);
+    }
+
+    public static Product GetBy(string code) {
+        return Products.FirstOrDefault(p => p.Code == code);
+    }
+
+    public static Product UpdateProduct(Product product) {
+        Product p = ProductRepository.GetBy(product.Code);
+        p.Name = product.Name;
+        return p;
+    }
+
+    public static void RemoveProduct(Product product) {
+        Products.Remove(product);
+    }
+}
+
+public class Product {
+    public string Code { get; set; }
+    public string Name { get; set; }
+}
